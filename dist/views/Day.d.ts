1c3941a108fb8f696dd49d6db833f004d0cbde91
davidsharff <davesharff@gmail.com>
2022-06-07 12:18:43 -0500

    Compile last commit

---
+++ dist/views/Day.d.ts
@@ -0,0 +1,9 @@
+import { CellRenderedProps, DayHours } from "../types";
+export interface DayProps {
+    startHour: DayHours;
+    endHour: DayHours;
+    step: number;
+    cellRenderer?(props: CellRenderedProps): JSX.Element;
+}
+declare const Day: () => JSX.Element;
+export { Day };
---
+++ dist/views/Week.d.ts
@@ -0,0 +1,12 @@
+import { CellRenderedProps, DayHours } from "../types";
+import { WeekDays } from "./Month";
+export interface WeekProps {
+    weekDays: WeekDays[];
+    weekStartOn: WeekDays;
+    startHour: DayHours;
+    endHour: DayHours;
+    step: number;
+    cellRenderer?(props: CellRenderedProps): JSX.Element;
+}
+declare const Week: () => JSX.Element;
+export { Week };
